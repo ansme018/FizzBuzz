6591e9e8a50d13b146cd3feea3a9de733fb49a4b
melanselmo@gmail.com <melissaanselmo@Melissas-MacBook-Air.local>
2015-12-05 07:55:15 -0500

    Fixed bugs

--- js/app.js
+++ js/app.js
@@ -3,18 +3,17 @@
 //Divisble by 3- "fizz"
 //Divisble by 5- "buzz"
 //Divisble by 3 and 5 "fizz buzz"
-//Neither just print number to DOM 
 
 var number = i;
 for (var i=1; i<=100; i++)
 	{if (i % 3 ==0 && i % 5 == 0) 
-		{console.log("fizzbuzz")};
+		{console.log("fizzbuzz")}
 	else if (i % 3 == 0) 
-		{console.log("fizz")};
+		{console.log("fizz")}
 	else if (i % 5 == 0) 
-		{console.log("buzz")};
+		{console.log("buzz")}
 	else {
-		console.log(i);
+		console.log(i)
 	}
 }
 
