0b3e84b7cb38687126b4571760dae9638a7aa5db
melanselmo@gmail.com <melissaanselmo@melissas-air.home>
2015-12-21 08:50:15 -0500

    Final fizzbuzz edits

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
 //FizzBuzz
-//1 to 100.  
-//Divisble by 3- "fizz"
-//Divisble by 5- "buzz"
-//Divisble by 3 and 5 "fizz buzz"
+//Rules: 1 to 100; Divisble by 3:fizz; Divisble by 5: "buzz"; Divisble by 3 and 5 "fizz buzz"
 
-var number = i;
-for (var i=1; i<=100; i++)
-	{if (i % 3 ==0 && i % 5 == 0) 
-		{console.log("fizzbuzz")}
-	else if (i % 3 == 0) 
-		{console.log("fizz")}
-	else if (i % 5 == 0) 
-		{console.log("buzz")}
-	else {
-		console.log(i)
-	}
-}
 
 	
+
+
+//
+//Loop through numbers
+
+$(document).ready(function() {
+	function countNumbers(maxValue){
+		for (var i = 1; i <=maxValue; i++)
+			{if (i % 3 ==0 && i % 5 == 0){ 
+				$('#fizzbuzz ul').append("FizzBuzz ");
+			} else if (i % 3 == 0){
+				$('#fizzbuzz ul').append("Fizz ");
+			}else if (i % 5 == 0) {
+				$('#fizzbuzz ul').append("Buzz ");
+			}else{
+				$('#fizzbuzz ul').append(i + " ");
+			}
+		}		
+	}
+
+	$('#SubmitButton').on('click', function(e) {
+		var fizznumber =$('#fizznumber').val();
+			
+		if (fizznumber % 1 ==0 && fizznumber <= 100) {
+			$('#fizzbuzz ul').empty();
+			countNumbers(fizznumber);
+			$('#fizznumber').val("");
+		}else {
+			alert("Type only numbers between 1 and 100.");
+			$("fizznumber").val("");
+		}
+	});
+
+});	
 
 
